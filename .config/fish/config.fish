945ab23ecab1d33a7dd71c2a6e5e14af1fa5e8e0
aiden <aidenrodrigues159@gmail.com>
2025-11-30 20:43:47 +0530

    add fish configs

---
+++ .config/fish/config.fish
@@ -0,0 +1,7 @@
+alias config='/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME'
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# Created by `pipx` on 2025-11-07 21:04:29
+set PATH $PATH /home/aiden/.local/bin
